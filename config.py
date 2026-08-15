bbf08b8e9a7df8db807694a658dd0f784ea4c576
likhith <250792636+likithbot@users.noreply.github.com>
2026-08-15 11:52:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,18 +4,18 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "36512703"))
+API_ID = int(os.environ.get("API_ID", "31512840"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "034042941920d3ad537e4936c56ae919")
+API_HASH = os.environ.get("API_HASH", "1d9a43e79e0a131b419ed66a34ee81bd")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8543568873:AAFD8PcRe9CcxLT6a97SVtcZGQZGJ1RnvRE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8543568873:AAH4pQ_v99cqbU9mUNiE5FOKu6YLQzWh65Y")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "NoPiracyBot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "8322551557"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5867783630"))
 # ------------------X------------------------------
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", "-5106839318"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002894650288"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://mohit18324:TxsMAm4VjmS0nQ74@cluster0.ynzyhrh.mongodb.net")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Likhith:Likhithbots@likhith.jm3wayg.mongodb.net/?retryWrites=true&w=majority&appName=likhith")
 # ------------------------------------------------
